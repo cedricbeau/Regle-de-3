41f4019b4398630efd80e4052dbffdfd53725620
cedricbeau <cedric7693@hotmail.fr>
2018-05-28 15:35:53 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,93 +1,97 @@
 'use strict';
 
-// Variables
-// Inputs
-let value1 = document.getElementById('value1')
-let value2 = document.getElementById('value2')
-let value3 = document.getElementById('value3')
-let valueX = document.getElementById('valueX')
-let inputs = document.querySelectorAll('input')
-//Btns
-let btnSubmit = document.querySelector('.btn-submit')
-let btnReset = document.querySelector('.btn-reset')
-
-// Detect number
-function isNumber(n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
-}
-
-//Calcul
-function regleTrois(val1, val2, val3) {
-  return Math.floor((val1 * val2) / val3)
-}
-
-//Danger boxes
-dangerBoxes()
-function dangerBoxes() {
+(function(){
 	
-  for(let i=0;i<inputs.length;i++){
-    let input = inputs[i]    
-    input.addEventListener('keyup', function(){
-      if(isNumber(input.value) === false) {
-	//Desactive submit
-	btnSubmit.style.opacity = .5
-	btnSubmit.style.pointerEvents = 'none'
-	//Add dangerbox  
-	input.parentNode.classList.add('danger')    
-      } else {
-	console.log(isNumber(input.value))
-	if(isNumber(value1.value) === true && isNumber(value1.value) === true && isNumber(value3.value) === true) {
-	  //Active submit
-	  btnSubmit.style.opacity = 1
-	  btnSubmit.style.pointerEvents = 'all'
-	}	      
-	if (input.parentNode.classList.contains('danger')) {
-	  //Remove dangerbox
-	  input.parentNode.classList.remove('danger')
+	// Variables
+	// Inputs
+	let value1 = document.getElementById('value1')
+	let value2 = document.getElementById('value2')
+	let value3 = document.getElementById('value3')
+	let valueX = document.getElementById('valueX')
+	let inputs = document.querySelectorAll('input')
+	//Btns
+	let btnSubmit = document.querySelector('.btn-submit')
+	let btnReset = document.querySelector('.btn-reset')
+	
+	// Detect number
+	function isNumber(n) {
+	  return !isNaN(parseFloat(n)) && isFinite(n);
 	}
-      }
-    })    
-  }
-
-}
-
-//Get Resultat
-function getResult() { 
 	
-  if(isNumber(regleTrois(value1.value, value2.value, value3.value) || valueX.innerText === 0) === false) {
-    valueX.innerText = ''
-    if(valueX.classList.contains('success')){
-      valueX.classList.remove('success')
-    }
-  } else {
-    valueX.innerText = regleTrois(value1.value, value2.value, value3.value)
-    valueX.classList.add('success')
-    btnReset.style.visibility = 'visible'
-  }
-  
-}
+	//Calcul
+	function regleTrois(val1, val2, val3) {
+	  return Math.floor((val1 * val2) / val3)
+	}
+	
+	//Alert boxes
+	function alertBoxes() {
+		
+		if(isNumber(this.value) === false) {
+			//Desactive submit
+			btnSubmit.style.opacity = .5
+			btnSubmit.style.pointerEvents = 'none'
+		  //Add dangerbox  
+		  this.parentNode.classList.add('danger')    
+		} else {
+		  console.log(isNumber(this.value))
+		  if(
+		  	isNumber(value1.value) === true &&
+		  	isNumber(value1.value) === true &&
+		  	isNumber(value3.value) === true) {
+			  //Active submit
+			  btnSubmit.style.opacity = 1
+			  btnSubmit.style.pointerEvents = 'all'
+			}	      
+			if (this.parentNode.classList.contains('danger')) {
+				//Remove dangerbox
+			  this.parentNode.classList.remove('danger')
+			}
+		}   
+	 
+	}
+	
+	//Get Resultat
+	function getResult() { 
+		
+	  if(isNumber(regleTrois(value1.value, value2.value, value3.value) || valueX.innerText === 0) === false) {
+	    valueX.innerText = ''
+	    if(valueX.classList.contains('success')){
+	      valueX.classList.remove('success')
+	    }
+	  } else {
+	    valueX.innerText = regleTrois(value1.value, value2.value, value3.value)
+	    valueX.classList.add('success')
+	    btnReset.style.visibility = 'visible'
+	  }
+	  
+	}
+	
+	// Reset function
+	function reset(){
+		// Supprime les valeurs des champs
+		value1.value = ''
+	  value2.value = ''
+	  value3.value = ''
+	  valueX.innerHTML = ''
+	  if(valueX.classList.contains('success')) {
+		  valueX.classList.remove('success')
+	  }
+	  // Désactive le bouton reset
+	  this.style.visibility = 'hidden'
+	  // Désactive le bouton submit
+	  btnSubmit.style.opacity = .5
+	  btnSubmit.style.pointerEvents = 'none'
+	}
+	
+	// Event listener
+	// keyup
+	for(let i=0;i<inputs.length;i++){
+		let input = inputs[i]    
+	  input.addEventListener('keyup', alertBoxes, false)    
+	}
+	// submit
+	btnSubmit.addEventListener('click', getResult, false)
+	// reset
+	btnReset.addEventListener('click', reset, false)
 
-// Reset function
-function reset(){
-  // Supprime les valeurs des champs
-  value1.value = ''
-  value2.value = ''
-  value3.value = ''
-  valueX.innerHTML = ''
-  if(valueX.classList.contains('success')) {
-    valueX.classList.remove('success')
-  }
-  // Désactive le bouton reset
-  this.style.visibility = 'hidden'
-  // Désactive le bouton submit
-  btnSubmit.style.opacity = .5
-  btnSubmit.style.pointerEvents = 'none'
-}
-
-// Event listener
-// submit
-btnSubmit.addEventListener('click', getResult, false)
-// reset
-btnReset.addEventListener('click', reset, false)
-
-
+})()
